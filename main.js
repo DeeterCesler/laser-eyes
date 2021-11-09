b963cc0e655536ed6fa4f47ec9b6b145e171b6b8
Deeter <deeter.cesler@gmail.com>
2021-11-08 22:48:20 -0500

    its shitty but it WORKS - lets you upload your own photo and laser eye it

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 var xPosition;
 var yPosition;
 var laser1 = document.getElementById('one')
-console.log(laser1)
 var laser2 = document.getElementById('two')
+let eye = "left"
 
 document.addEventListener("click", getClickPosition, false);
-// When the click happens, we have an event handler that will react to that click:
 
-let eye = "left"
 function toggleEye() {
     if (eye === "left") eye = "right"
     else eye = "left"
@@ -17,24 +15,16 @@ const placeLaser = (toggledEye) => {
     if (toggledEye === "left") {
         laser1.style.left = xPosition + "px"
         laser1.style.top = yPosition + "px"
-        console.log('left')
     } else {
         laser2.style.left = xPosition + "px"
         laser2.style.top = yPosition + "px"
-        console.log('right')
     }
 }
 
 function getClickPosition(e) {
     xPosition = e.clientX - 90;
     yPosition = e.clientY - 480;
-    
-    console.log(xPosition)
-    console.log(yPosition)
 
     placeLaser(eye)
     toggleEye();
 }
-
-
-console.log(laser1.style.left)
